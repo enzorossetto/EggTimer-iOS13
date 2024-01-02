e21678a721fdb5db23266cd7b78c80d8cf348141
Enzo Rossetto <enzo.rossetto@ifood.com.br>
2024-01-02 14:44:01 -0300

    feat: implement timer count as progress bar component when egg type is pressed

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,7 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
     
+    var timer: Timer?
+    var secondsPassed = 0
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
 
-
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        if let hardness = sender.currentTitle, let totalTime = eggTimes[hardness] {
+            if timer == nil || !timer!.isValid {
+                secondsPassed = 0
+                titleLabel.text = hardness
+                progressBar.progress = 0.0
+                
+                timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
+                    print("\(self.secondsPassed) seconds passed")
+                    
+                    self.progressBar.progress = Float(self.secondsPassed) / Float(totalTime)
+                    
+                    if self.secondsPassed == totalTime {
+                        self.titleLabel.text = "Done!"
+                        timer.invalidate()
+                    } else {
+                        self.secondsPassed += 1
+                    }
+                }
+            }
+        } else {
+            print("Error")
+        }
+    }
+    
 }
